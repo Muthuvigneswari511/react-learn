d009e10c3ac6dfda363f9fa6f2e97d6c4d88d775
muthu <muthua8930@gmail.com>
2026-04-17 23:33:23 +0530

    suggestion and post page updated

--- Instagram-clone/src/Sidebar.jsx
+++ Instagram-clone/src/Sidebar.jsx
@@ -1,20 +1,20 @@
 const Sidebar=()=>{
     return(
-        <div className="m-3">
+        <div className="m-3 position-fixed">
         <div className="d-flex flex-column gap-3">
             <img src="./src/assets/text_insta.webp" className="img"/>
-            <div><i class="bi bi-house-door-fill"></i>Home</div>
-            <div><i class="bi bi-search"></i>Search</div>
-            <div><i class="bi bi-compass"></i>Explore</div>
-            <div><i class="bi bi-play-btn-fill"></i>Reels</div>
-            <div><i class="bi bi-send"></i>Messages</div>
-            <div><i class="bi bi-heart"></i>Notifications</div>
-            <div><i class="bi bi-plus"></i>Create</div>
-            <div><i class="bi bi-person-circle"></i>Profile</div> 
+            <div><i className="bi bi-house-door-fill"></i>Home</div>
+            <div><i className="bi bi-search"></i>Search</div>
+            <div><i className="bi bi-compass"></i>Explore</div>
+            <div><i className="bi bi-play-btn-fill"></i>Reels</div>
+            <div><i className="bi bi-send"></i>Messages</div>
+            <div><i className="bi bi-heart"></i>Notifications</div>
+            <div><i className="bi bi-plus"></i>Create</div>
+            <div><i className="bi bi-person-circle"></i>Profile</div> 
         </div>
         <div className="position-fixed bottom-0 mb-2 d-flex flex-column gap-3">
-            <div><i class="bi bi-threads"></i>Threads</div>
-            <div><i class="bi bi-list"></i>More</div>
+            <div><i className="bi bi-threads"></i>Threads</div>
+            <div><i className="bi bi-list"></i>More</div>
         </div>
     </div>    
     );
--- Instagram-clone/src/Suggestions.jsx
+++ Instagram-clone/src/Suggestions.jsx
@@ -1,6 +1,54 @@
-const Suggestions=()=>{
+import { use, useEffect, useState } from "react"
+
+const Suggestions = () => {
+    const [profile, setProfile] = useState(null);
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/profile')
+        .then((data)=>data.json())
+        .then((data)=>setProfile(data))
+        .catch(err=>console.log(err))
+    }, [])
+    useEffect(() => {
+        fetch('http://localhost:3000/suggestion')
+        .then((data)=>data.json())
+        .then((data)=>setSuggestions(data))
+        .catch(err=>console.log(err))
+    }, [])
     return (
-        <></>
-    );
+        <div>
+            <div className="suggestions m-4 ">
+                {profile?
+            <div className="d-flex">
+                <img className="dp rounded-circle"
+                src={profile.profilePic}/>
+                <h5>{profile.username}</h5>
+                <small className="ms-auto text-primary">Switch</small>
+            </div>
+            :<p>Loading</p>}
+            <div className="d-flex">
+                <p>Suggested for you</p>
+                <b className="ms-auto">See All</b>
+            </div>
+            {suggestions.length > 0 ? (
+                <div>{suggestions.map((suggestion) => (
+                    <div  key={suggestion.id}>
+                        <div className="d-flex">
+                            <img className="dp rounded-circle" src={suggestion.profilePic} alt="profile pic" />
+                            <h5>{suggestion.username}</h5>
+                            <p className="text-primary ms-auto">Follow</p>
+                        </div>
+                       
+                       
+                        
+                    </div>
+                ))}</div>
+            ) : (
+                <div>Loading</div>
+            )}
+            </div>
+        </div>
+                    );
 }
-export default Suggestions;
+                        export default Suggestions;
