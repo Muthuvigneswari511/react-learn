7f98d305cb08d0d9669e0365d9c91af65ac87487
muthu <muthua8930@gmail.com>
2026-04-19 11:16:33 +0530

    stories components worked completed

--- Instagram-clone/src/Stories.jsx
+++ Instagram-clone/src/Stories.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const Stories = () => {
     const [stories, setStories] = useState([]);
+
+    const navigate=useNavigate();
+
+    let tot=0;
 
     useEffect(() => {
         fetch("http://localhost:3000/story")
@@ -12,9 +17,10 @@ const Stories = () => {
 
     return (
         <div className="story d-flex">
+            <div className="d-none">{tot=stories.length}</div>
             {stories.length > 0 ? (
                 stories.map((story) => (
-                    <div className="mx-1"  key={story.id}>
+                    <div className="mx-1"  key={story.id} onClick={()=>{navigate(`/story/${story.id}/${tot}`)}}>
                         <div className="gradient-border">
                         <img className="story-dp rounded-circle" src={story.profilePic} alt="profile pic" />
                         </div>
--- Instagram-clone/src/ViewStory.jsx
+++ Instagram-clone/src/ViewStory.jsx
@@ -1,22 +1,35 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams,Link, useNavigate } from "react-router";
 
 const ViewStory=()=>{
-    const {id}=useParams();
+    const {id,tot}=useParams();
 
     const [story,setStory]=useState(null);
+
+    const navigate=useNavigate();
 
     useEffect(()=>{
         fetch(`http://localhost:3000/story/${id}`)
         .then(data=>data.json())
-        .then(res=>setStory(res))
+        .then(data=>
+            {
+                console.log(data);
+                setStory(data);
+            })
         .catch(err=>console.log(err))
     },[id]);
+    if(id>tot|| id<1){
+        navigate('/')
+    }
     return(
         <div>
         {story?
-        <div>
-            <img src={story.image} alt="story"/>
+        <div className="d-flex justify-content-center align-items-center">
+            <Link to={`/story/${Number(id) - 1 }/${tot}`}><i className="bi bi-arrow-left-circle-fill"></i>
+            </Link>
+            
+            <img className="vh-100" src={story.image} alt="story"/>
+            <Link to={`/story/${Number(id)+1}/${tot}`}><i className="bi bi-arrow-right-circle-fill"></i></Link>
         </div>
         :<div>Loading</div>}
         </div>
